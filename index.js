5f7304028324a0a42c1de8b0b1deff6369d04fec
Tony Pham <tnpham@valdosta.edu>
2022-04-27 20:18:22 -0400

    commit tree e2056c0591db80319bfd095e4ceaae6c99e8815f

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31;
+
+let num2 = 2;
+
+const multiply = parseInt(num1) * parseInt(num2)
+
+
+
+const random = Math.floor(Math.random() * 10) +1;
+
+let num3 = 29;
+let num4 = 5;
+
+const mod = (num3) % (num4)
+
+
+const max = Math.max(1,2,8,20,7,9,19);
